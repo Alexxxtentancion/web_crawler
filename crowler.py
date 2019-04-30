15547e7092caf5d96de15e87f6adc737edf0cd91
Artem Alexeev <petrichlast@mail.ru>
2019-04-30 23:03:16 +0300

    upd

--- crowler.py
+++ crowler.py
@@ -4,29 +4,30 @@ from urllib.parse import urljoin, urldefrag, urlparse
 import aiohttp
 from aioelasticsearch import Elasticsearch
 from bs4 import BeautifulSoup
-import requests
-start_url = 'http://fargo-online.net/'
+
+start_url = 'http://site-of-thrones.ru/'
 index_url = start_url.translate({ord(i): None for i in """"[*\>:\<'"|/?]"""})
+
 
 class Crowler:
     def __init__(self, url, rps, max_pages):
         self.url = url
-        self.id =0
+        self.id = 0
         self.rps = rps
         self.links = [url]
         self.domain = '{uri.scheme}://{uri.netloc}/'.format(uri=urlparse(start_url))
 
     @staticmethod
-    def connect_elasticsearch():
+    async def connect_elasticsearch():
         _es = None
         _es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-        if _es.ping():
+        if await _es.ping():
             print('Yay Connect')
         else:
             print('Awww it could not connect!')
         return _es
 
-    def create_index(self, es_object, index_name):
+    async def create_index(self, es_object, index_name):
         created = False
         settings = {
             "settings": {
@@ -49,9 +50,9 @@ class Crowler:
         }
 
         try:
-            if not es_object.indices.exists(index_name):
+            if not await es_object.indices.exists(index_name):
                 # Ignore 400 means to ignore "Index Already Exist" error.
-                es_object.indices.create(index=index_name, ignore=400, body=settings)
+                await es_object.indices.create(index=index_name, ignore=400, body=settings)
                 print('Created Index')
             created = True
         except Exception as ex:
@@ -67,10 +68,10 @@ class Crowler:
         links = [urldefrag(x)[0] for x in absolute_links if x.startswith(self.domain)]
         return list(set(links)), soup
 
-    async def store_record(self, elastic_object, index_name,_id, record):
+    async def store_record(self, elastic_object, index_name, _id, record):
         is_stored = True
         try:
-            outcome = await elastic_object.index(index=index_name,doc_type='page',body=record,id=_id)
+            outcome = await elastic_object.index(index=index_name, doc_type='page', body=record, id=_id)
             # print(outcome)
         except Exception as ex:
             print('Error in indexing data')
@@ -81,49 +82,47 @@ class Crowler:
 
     async def spider(self, start_link, es, i, session):
         links_to_visit = [start_link]
-        async with session.get(start_link) as response:
-            for link in links_to_visit:
-                new_links,soup = await self.get_links(await response.text())
-                if self.create_index(es, index_url):
-                    self.id+=1
-                    await self.store_record(es, index_url,self.id, {"link": link, "text": await self.beautify_text(soup)})
-                    print('Data indexed successfully')
-                for new_link in new_links:
-                    if new_link not in self.links and new_link not in links_to_visit:
-                        self.links.append(new_link)
-                        links_to_visit.append(new_link)
-                print(i,self.links)
+        for link in links_to_visit:
+            async with session.get(link) as response:
+                new_links, soup = await self.get_links(await response.text())
+            if await self.create_index(es, index_url):
+                self.id += 1
+                await self.store_record(es, index_url, self.id, {"link": link, "text": await self.beautify_text(soup)})
+            for new_link in new_links:
+                if (new_link not in self.links) and (new_link not in links_to_visit):
+                    self.links.append(new_link)
+                    links_to_visit.append(new_link)
 
-                # await asyncio.sleep(1/self.rps)
+        # await asyncio.sleep(1/self.rps)
 
     async def main(self):
-        es = self.connect_elasticsearch()
+        es = await self.connect_elasticsearch()
         tasks = []
         async with aiohttp.ClientSession() as session:
             async with session.get(start_url) as resp:
                 links_to_visit, soup = await self.get_links(await resp.text())
-                if self.create_index(es, index_url):
-                    await self.store_record(es, index_url,self.id, {"link": start_url, "text": await self.beautify_text(soup)})
+                if await self.create_index(es, index_url):
+                    await self.store_record(es, index_url, self.id,
+                                            {"link": start_url, "text": await self.beautify_text(soup)})
                     print('Data indexed successfully')
 
                 self.links.append(links_to_visit)
-                print(links_to_visit)
+                for i in links_to_visit: print(i)
 
             for i, link in enumerate(links_to_visit):
-                task = asyncio.create_task(self.spider(link, es, i,session))
+                task = asyncio.ensure_future(self.spider(link, es, i, session))
                 tasks.append(task)
+            # for task in (asyncio.as_completed(tasks)):
+            #     await task
             await asyncio.gather(*tasks)
         await es.close()
-
-
-
 
     @staticmethod
     async def beautify_text(soup):
         for script in soup(["script", "style"]):
             script.extract()  # rip it out
-        text = soup.get_text()
         await asyncio.sleep(0)
+        text = soup.get_text()
         lines = (line.strip() for line in text.splitlines())
         chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
         text = '\n'.join(chunk for chunk in chunks if chunk)
