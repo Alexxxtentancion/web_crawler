184dab3e24002132a5c305e56d57963fb0ac3666
Artem Alexeev <petrichlast@mail.ru>
2019-05-09 16:27:47 +0300

    'signals'

--- api/main.py
+++ api/main.py
@@ -3,19 +3,15 @@ from aiohttp import web
 
 from api.view import search
 
-async def connect_elasticsearch():
-    _es = None
-    _es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-    if await _es.ping():
-        print('Yay Connect')
-    else:
-        print('Awww it could not connect!')
-    return _es
+async def connect_elasticsearch(app):
+    app['es'] = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+
 
 
 if __name__ == '__main__':
-    es = connect_elasticsearch()
     app = web.Application()
+    app.on_startup.append(connect_elasticsearch)
     app.router.add_post('/api/search', search)
     web.run_app(app)
 
--- api/view.py
+++ api/view.py
@@ -3,13 +3,13 @@ import json
 from crawler import index_url
 
 
-async def search(request,es):
+async def search(request,app):
     try:
         query = request.query['q']
         limit = request.query['limit']
         offset = request.query['offset']
         search_object = {'query': {'match': {'text': query}},'size':limit,'from':offset}
-        res = await es.search(index=index_url, body=json.dumps(search_object))
+        res = await app['es'].search(index=index_url, body=json.dumps(search_object))
         return web.Response(text=json.dumps(res.get('hits').get('hits')), status=200)
     except Exception as e:
         response_obj = { 'status' : 'failed', 'reason': str(e) }
